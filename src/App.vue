68c0addfb0c41998cc111036b3eb766d6f482bb5
itmajun <itmajun@163.com>
2018-02-09 18:04:05 +0800

    wexx 不支持, 删除transition

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
-    <transition name="router-fade" mode="out-in">
         <router-view style="flex:1"></router-view>
-    </transition>
 </template>
 
 <script>
@@ -12,24 +10,3 @@
         }
     }
 </script>
-
-<style scoped>
-    .wrapper {
-        justify-content: center;
-        align-items: center;
-    }
-    .logo {
-        width: 424px;
-        height: 200px;
-    }
-    .greeting {
-        margin-top: 70px;
-        font-size: 50px;
-        color: #41B883;
-    }
-    .message {
-        margin: 30px;
-        font-size: 32px;
-        color: #727272;
-    }
-</style>
